dd8c25ab6b02491eeefde1be3a8fbf5847cf362e
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-06-22 18:34:39 +0200

    Cleaner code 3

--- FlyweightPattern-OOM/FlyweightFactory.cs
+++ FlyweightPattern-OOM/FlyweightFactory.cs
@@ -11,35 +11,28 @@ namespace FlyweightPattern_OOM
         public Dictionary<string, Image> images = new Dictionary<string, Image>();
         public Image getImage2(PictureBox picture, bool deleted = false)
         {
-            int flag = 0;
             OpenFileDialog f = new OpenFileDialog();
             f.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
             if (f.ShowDialog() == DialogResult.OK)
             {
                 string directoryName;
-                Image value = null;
                 directoryName = Path.GetFullPath(f.FileName);
                 //console writeline napravljen radi testa, da vidim da li je path tocan
                 Console.WriteLine("path to that current picture is: {0}", directoryName);
                 if (images.ContainsKey(directoryName))
                 {
-                        flag = 1;
-                        value = images[directoryName];
-                        return value;
+
+                        return images[directoryName];
                 }
                 if (deleted)
                 {
                     return null;
                 }
-                value = Image.FromFile(f.FileName);
-                if (flag == 0)
+                using (Image image = Image.FromFile(directoryName))
                 {
-                    using (Image image = Image.FromFile(directoryName))
-                    {
-                        value = new Bitmap(image);
+                        Image value = new Bitmap(image);
                         images.Add(directoryName, value);
                         return value;
-                    }
                 }
             }
             //ovaj return se nece nikada izvrsiti
