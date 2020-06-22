126de2e65f76c6cbd03c2ff30a678a913dc626f0
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-06-22 17:42:19 +0200

    Cleaner code, more localized variables

--- FlyweightPattern-OOM/FlyweightFactory.cs
+++ FlyweightPattern-OOM/FlyweightFactory.cs
@@ -14,36 +14,32 @@ namespace FlyweightPattern_OOM
     class FlyweightFactory
     {
         public Dictionary<string, Image> images = new Dictionary<string, Image>();
-        Image imgfile;
-        string directoryName;
-        int flag=0;
         public Image getImage2(PictureBox picture, bool deleted = false)
         {
+            int flag = 0;
+            string directoryName;
             Image value = null;
             OpenFileDialog f = new OpenFileDialog();
             f.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
             if (f.ShowDialog() == DialogResult.OK)
             {
-                value = Image.FromFile(f.FileName);
+                //value = Image.FromFile(f.FileName);
                 directoryName = Path.GetFullPath(f.FileName);
                 //console writeline napravljen radi testa, da vidim da li je path tocan
                 Console.WriteLine("path to that current picture is: {0}", directoryName);
-                for (int i = 0; i < images.Count; i++)
+                if (images.ContainsKey(directoryName))
                 {
-                    if (images.ContainsKey(directoryName))
-                    {
                         flag = 1;
                         value = images[directoryName];
                         return value;
-                    }
                 }
+                value = Image.FromFile(f.FileName);
                 if (deleted)
                 {
                     return null;
                 }
                 if (flag == 0)
                 {
-                    //value = imgfile;
                     images.Add(directoryName, value);
                     foreach (var item in images)
                     {
