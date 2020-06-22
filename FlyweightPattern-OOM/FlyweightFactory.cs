84d326050e562475b1cee07ed2db10758ce8045a
Bruno Čale <50056973+brcale@users.noreply.github.com>
2020-06-22 18:20:42 +0200

    Cleaner code 2

--- FlyweightPattern-OOM/FlyweightFactory.cs
+++ FlyweightPattern-OOM/FlyweightFactory.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FlyweightPattern_OOM
@@ -17,13 +12,12 @@ namespace FlyweightPattern_OOM
         public Image getImage2(PictureBox picture, bool deleted = false)
         {
             int flag = 0;
-            string directoryName;
-            Image value = null;
             OpenFileDialog f = new OpenFileDialog();
             f.Filter = "Image Files (*.jpg;*.jpeg;.*.gif;)|*.jpg;*.jpeg;.*.gif";
             if (f.ShowDialog() == DialogResult.OK)
             {
-                //value = Image.FromFile(f.FileName);
+                string directoryName;
+                Image value = null;
                 directoryName = Path.GetFullPath(f.FileName);
                 //console writeline napravljen radi testa, da vidim da li je path tocan
                 Console.WriteLine("path to that current picture is: {0}", directoryName);
@@ -33,23 +27,23 @@ namespace FlyweightPattern_OOM
                         value = images[directoryName];
                         return value;
                 }
-                value = Image.FromFile(f.FileName);
                 if (deleted)
                 {
                     return null;
                 }
+                value = Image.FromFile(f.FileName);
                 if (flag == 0)
                 {
-                    images.Add(directoryName, value);
-                    foreach (var item in images)
+                    using (Image image = Image.FromFile(directoryName))
                     {
-                        Console.WriteLine(item.Key);
+                        value = new Bitmap(image);
+                        images.Add(directoryName, value);
+                        return value;
                     }
-                    return value;
                 }
             }
             //ovaj return se nece nikada izvrsiti
-            return value;
+            return null;
         }
 
     }
